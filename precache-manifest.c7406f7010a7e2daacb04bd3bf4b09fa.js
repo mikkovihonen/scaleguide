db261e638b0509346672efeaffee47b42344cbb5
Mikko Vihonen <mikko.vihonen@iki.fi>
2018-11-28 12:38:24 +0200

    Updates

---
+++ precache-manifest.c7406f7010a7e2daacb04bd3bf4b09fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a51d2337db17e97697ffb2a6d26f7d2",
+    "url": "/scaleguide/static/media/background.9a51d233.png"
+  },
+  {
+    "revision": "3794cbd89375bd4d4dba",
+    "url": "/scaleguide/static/js/runtime~main.3794cbd8.js"
+  },
+  {
+    "revision": "3f6a9464b6447d346a9d",
+    "url": "/scaleguide/static/js/main.3f6a9464.chunk.js"
+  },
+  {
+    "revision": "93c177db0ec9f1865962",
+    "url": "/scaleguide/static/js/1.93c177db.chunk.js"
+  },
+  {
+    "revision": "3f6a9464b6447d346a9d",
+    "url": "/scaleguide/static/css/main.724653d3.chunk.css"
+  },
+  {
+    "revision": "fa5a6cea0bd9f49600266f0a243642ce",
+    "url": "/scaleguide/index.html"
+  }
+];
